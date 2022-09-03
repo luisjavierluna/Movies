811b618c82a8408e2ffc89b3024613c49bf3a7b0
luisjavierluna <javierlunaluis@gmail.com>
2022-09-03 16:35:42 -0500

    Added first UI elements and the array thar represents the database data

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  
+  movies;
+  
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.movies = [{
+        movieTitle: 'Spider-Man',
+        releaseDate: new Date(),
+        price: 1400.99
+      },
+      {
+        movieTitle: 'Moana',
+        releaseDate: new Date('2016-11-14'),
+        price: 300.99
+      }]
+    }, 3000);
+  }
 }
