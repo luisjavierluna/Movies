16bfdc631af4baa0c0300c4bdc7c3c64e7949d17
luisjavierluna <javierlunaluis@gmail.com>
2022-09-21 11:28:50 -0500

    Added component to filter movies and genres: PART 2

--- frontend/src/app/movies/filter-movies/filter-movies.component.ts
+++ frontend/src/app/movies/filter-movies/filter-movies.component.ts
@@ -18,17 +18,51 @@ export class FilterMoviesComponent implements OnInit {
     {id: 3, name: 'Comedy'}
   ]
 
+  movies = [
+    {title: 'Spider-Man: No Way Home', inTheaters: false, futureReleases: true, genres: [1, 2], poster: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSgSLYeR2r0eYlaVxhkZ65bPtHlFrVlxo5APFkWlaaHlqEJIoa3'},
+    {title: 'Moana', inTheaters: true, futureReleases: false, genres: [3], poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0I_NLZX7ywKTPOE6dP5IORzdjWJvBLz7ZLeyVR0HHuXieSF6l'},
+    {title: 'Inception', inTheaters: false, futureReleases: false, genres: [1, 3], poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfzT9O0IBDumCXZjeAIhSYLANDT5ip_dwMNKr9-fBQfaSuD3sv'},
+  ]
+
+  moviesOriginal = this.movies;
+
+  originalForm = {
+    title: '',
+    genreId: 0,
+    futureReleases: false,
+    inTheaters: false
+  }
+
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      title: '',
-      genreId: 0,
-      futureReleases: false,
-      inTheaters: false
-    })
+    this.form = this.formBuilder.group(this.originalForm)
+
+    this.form.valueChanges
+      .subscribe(values => {
+        this.movies = this.moviesOriginal;
+        this.searchMovies(values)
+      })
+  }
+
+  searchMovies(values: any) {
+    if (values.title) {
+      this.movies = this.movies.filter(movie => movie.title.indexOf(values.title) !== -1)
+    }
+
+    if(values.genreId !== 0){
+      this.movies = this.movies.filter(movie => movie.genres.indexOf(values.genreId) !== -1)
+    }
+
+    if(values.futureReleases){
+      this.movies = this.movies.filter(movie => movie.futureReleases)
+    }
+
+    if(values.inTheaters){
+      this.movies = this.movies.filter(movie => movie.inTheaters)
+    }
   }
 
   clean(){
-    
+    this.form.patchValue(this.originalForm);
   }
 
 }
