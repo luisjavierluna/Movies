e46015e2f9e294aea9401aa2dac16ab28a1bf24f
luisjavierluna <javierlunaluis@gmail.com>
2022-10-06 15:46:01 -0500

    Added genres model and controller main methods

---
+++ backend/backend/Controllers/GenresController.cs
@@ -0,0 +1,48 @@
+﻿using backend.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly ILogger<GenresController> logger;
+
+        public GenresController(ILogger<GenresController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Genre>> Get()
+        {
+            return new List<Genre>() { new Genre() { Id = 1, Name = "Comedy" } };
+        }
+
+        [HttpGet("{Id:int}")]
+        public Task<ActionResult<Genre>> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genre genre)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public ActionResult Put()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
